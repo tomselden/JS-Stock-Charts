ddde5792d001a67ed68520674632bd9088ffbbfa
Thomas Selden <thomasselden42@gmail.com>
2021-11-17 16:59:48 -0500

    finished first graph, but couldn't figure out how to do bar graph

--- public/index.js
+++ public/index.js
@@ -1,9 +1,63 @@
 async function main() {
+  const timeChartCanvas = document.querySelector("#time-chart");
+  const highestPriceChartCanvas = document.querySelector(
+    "#highest-price-chart"
+  );
+  const averagePriceChartCanvas = document.querySelector(
+    "#average-price-chart"
+  );
 
-    const timeChartCanvas = document.querySelector('#time-chart');
-    const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
-    const averagePriceChartCanvas = document.querySelector('#average-price-chart');
+  // let response = await fetch('https://api.twelvedata.com/stocks?symbol=GME&MSFT&DIS&BNTX')
+  // let result = response.json()
+  // console.log(result)
 
+  const { GME, MSFT, DIS, BNTX } = mockData;
+
+  const stocks = [GME, MSFT, DIS, BNTX];
+
+  stocks.forEach((stock) => stock.values.reverse());
+
+  // Time Chart
+  new Chart(timeChartCanvas.getContext("2d"), {
+    type: "line",
+    data: {
+      labels: stocks[0].values.reverse().map((value) => value.datetime),
+      datasets: stocks.map((stock) => ({
+        label: stock.meta.symbol,
+        data: stock.values.reverse().map((value) => parseFloat(value.high)),
+        backgroundColor: getColor(stock.meta.symbol),
+        borderColor: getColor(stock.meta.symbol),
+      })),
+    },
+  });
+
+  function getColor(stock) {
+    if (stock === "GME") {
+      return "rgba(61, 161, 61, 0.7)";
+    }
+    if (stock === "MSFT") {
+      return "rgba(209, 4, 25, 0.7)";
+    }
+    if (stock === "DIS") {
+      return "rgba(18, 4, 209, 0.7)";
+    }
+    if (stock === "BNTX") {
+      return "rgba(166, 43, 158, 0.7)";
+    }
+  }
+  var config = {
+    type: "bar",
+    data: {
+      label: ["GME", "MSFT", "DIS", "BNTX"],
+      datasets: [
+        {
+          label: "Highest Priced Stock",
+          data: [0, 50, 100, 150, 200, 250, 300, 350],
+        },
+      ],
+    },
+  };
+  new Chart(highestPriceChartCanvas, config);
 }
 
-main()
+main();
